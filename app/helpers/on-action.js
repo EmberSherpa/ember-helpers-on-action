af5ac71ec191f2d6c869a7bb788d9a443e963283
Taras Mankovski <taras@quandl.com>
2014-10-06 11:39:50 -0400

    Moved helper handler function into addon

--- app/helpers/on-action.js
+++ app/helpers/on-action.js
@@ -1,40 +1,5 @@
 import Ember from 'ember';
-import DOMAction from 'ember-dom-actions/action';
+import DOMActionHelper from 'ember-dom-actions/helper';
 
-export default Ember.Handlebars.makeBoundHelper(onActionHelper);
-var get = Ember.get;
+export default Ember.Handlebars.makeBoundHelper(DOMActionHelper);
 
-/**
- * Bind an event emitter to an action on context via a Handlebars Helper
- *
- * {{on-action 'paginate' trigger="jump-to-top"}}
- *
- * @param {string} actionName
- * @param {object} options
- */
-function onActionHelper(actionName, options){
-  Ember.assert("{{on-action}} actionName parameter is missing.", actionName != null);
-
-  var context = get(options, 'hash.context') || this;
-  var view = get(options, 'data.view.parentView');
-  var eventName = get(options, 'hash.trigger') || actionName;
-  var actions = get(context, '_actions') || {};
-  var action;
-
-  if (actions.hasOwnProperty(actionName)) {
-    action = actions[actionName];
-  } else {
-    var bubble = options.hash.bubble || false;
-    action = function defaultAction() {
-      return bubble;
-    };
-    actions[actionName] = action;
-  }
-  action = action instanceof DOMAction ? action : DOMAction.convert(context, actionName, eventName);
-
-  /**
-   * Register view with the action.
-   * When action is triggered, the event will be triggered on this view.
-   */
-  action.register(view);
-}
